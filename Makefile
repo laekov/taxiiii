3dec6e650883151aca0a1059decb03e623b1ed55
laekov <laekov.h@gmail.com>
2019-05-22 13:11:15 +0800

    main part finish but not tested

--- Makefile
+++ Makefile
@@ -3,10 +3,13 @@ ccflags=-std=c++11 -g
 libpath=-I/home/laekov/.local/include -L/home/laekov/.local/lib
 objs=bin/gtree.o bin/gptree.o
 
+default : data/gtree_build
 
-all : bin/test
 
-bin/test : src/test.cc $(objs)
+data/gtree_build : src/gtree_build.cpp
+	g++ -std=c++0x -O3 -fopenmp $< -I/home/laekov/.local/include -L/home/laekov/.local/lib/ -lmetis -o $@
+
+bin/% : src/%.cc $(objs)
 	$(cc) $(ccflags) $< $(objs) -o ./$@ $(libpath) -lmetis
 
 bin/%.o : src/%.cc
---
+++ src/findtaxi.cc
@@ -0,0 +1,217 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <sstream>
+
+#include "gtree.hh"
+#include "gptree.hh"
+
+
+using namespace std;
+
+
+namespace taxiiii {
+
+const int max_neighbor = 1000;
+const int num_res = 5;
+const int int_inf = 0x3f3f3f3f;
+const int max_dist = 1e5; // 10km = 1e5dm
+
+struct Router {
+	int d1, pos, l;
+	vector<int> dests;
+	vector<int> d1_opt;
+	vector<vector<int> > dists;
+
+	int dfs_min;
+	vector<int> dfs_opt, dfs_stack;
+
+	void DFS(int k, int u, int vis, int dist) {
+		if (dist >= dfs_min) {
+			return;
+		}
+		if (vis == (1 << k) - 1) {
+			dfs_min = dist;
+			dfs_opt = dfs_stack;
+			return;
+		}
+		for (int v = 0; v < k; ++v) {
+			if (!((1 << v) & vis)) {
+				dfs_stack.push_back(dests[v]);
+				DFS(k, v, vis | (1 << v), dist + dists[u][v]);
+				dfs_stack.pop_back();
+			}
+		}
+	}
+
+	int getDist(int k, int src) {
+		dfs_min = int_inf;
+		dfs_stack.clear();
+		for (int i = 0; i < k; ++i) {
+			dfs_stack.push_back(dests[i]);
+			DFS(k, i, 1 << i, gptree::query(src, dests[i]));
+			dfs_stack.pop_back();
+		}
+		return dfs_min;
+	}
+
+	Router(int pos_, vector<int>& dests_) {
+		pos = pos_;
+		dests = dests_;
+		l = (int)dests.size();
+		dests.push_back(0);
+
+		dists.resize(l + 1);
+		for (int i = 0; i <= l; ++i) {
+			dists[i].resize(l + 1);
+			dists[i][i] = 0;
+			if (i == l) {
+				break;
+			}
+			for (int j = 0; j < i; ++j) {
+				dists[i][j] = dists[j][i] = gptree::query(dests[i], dests[j]);
+			}
+		}
+		d1 = getDist(l, pos);
+		d1_opt = dfs_opt;
+	}
+
+	int calcD3(int src, int dst, vector<int>* opt) {
+		dests[l] = dst;
+		for (int i = 0; i < l; ++i) {
+			dists[i][l] = dists[l][i] = gptree::query(dests[i], dst);
+		}
+		int d3(getDist(l + 1, src));
+		if (opt != 0) {
+			*opt = dfs_opt;
+		}
+		return d3;
+	}
+};
+
+struct Point {
+	double x, y;
+	Point(double x_ = 0, double y_ = 0): x(x_), y(y_) {} 
+};
+
+struct Taxi {
+	int id, k, pos;
+	Point loc;
+	vector<int> dests;
+	vector<Point> dest_loc;
+	Router* rtr;
+};
+
+vector<vector<Taxi> > taxi_on_node;
+
+int n;
+
+void readTaxis(string filename) {
+	taxi_on_node.resize(n);
+	FILE* fin(fopen(filename.c_str(), "r"));
+	int id, k, cnt(0);
+	while (fscanf(fin, "%d%d", &id, &k) != EOF) {
+		Taxi t;
+		t.id = id, t.k = k;
+		t.rtr = 0;
+		fscanf(fin, "%lf,%lf,%d", &t.loc.x, &t.loc.y, &t.pos);
+		t.dests.resize(k);
+		t.dest_loc.resize(k);
+		for (int j = 0; j < k; ++j) {
+			fscanf(fin, "%lf,%lf,%d", &t.dest_loc[j].x, &t.dest_loc[j].y, &t.dests[j]);
+		}
+		taxi_on_node[t.pos].push_back(t);
+		++cnt;
+	}
+	fclose(fin);
+	printf("%d taxis loaded\n", cnt);
+}
+
+void init() {
+	// gtree::init_all();
+	// puts("GTree initialized");
+	n = gptree::init_all();
+	puts("GPTree initialized");
+	readTaxis("car.txt");
+}
+
+int is_taxi_ok(Taxi& t, int pos, int dest, int d2, int d4, vector<string>& res) {
+	if (t.k == 4) {
+		return 0;
+	}
+	if (t.rtr == 0) {
+		t.rtr = new Router(t.pos, t.dests);
+	}
+	vector<int> rt;
+	int d1(t.rtr->d1);
+	int d3(t.rtr->calcD3(pos, dest, &rt));
+	if (d2 + d3 - d1 > max_dist) {
+		return 0;
+	}
+	if (d3 - d4 > max_dist) {
+		return 0;
+	}
+	string res_json;
+	ostringstream sou(res_json);
+	sou << "{";
+	sou << "\"taxi_id\":" << t.id << ",";
+	sou << "\"d1\":" << d1 << ",";
+	sou << "\"d2\":" << d2 << ",";
+	sou << "\"d3\":" << d3 << ",";
+	sou << "\"d4\":" << d4 << ",";
+	sou << "\"route_orig\":[";
+	for (auto i : t.rtr->d1_opt) {
+		sou << i << ",";
+	}
+	sou << "],";
+	sou << "\"route_new\":[";
+	for (auto i : rt) {
+		sou << i << ",";
+	}
+	sou << "]";
+	sou << "}";
+	res.push_back(res_json);
+	return 1;
+}
+
+string find(int pos, int dest) {
+	auto cands(gtree::knn_query(pos, max_neighbor));
+	vector<string> res;
+	int d4(gptree::query(pos, dest));
+	for (auto& c : cands) {
+		for (auto& t : taxi_on_node[c.id]) {
+			if (is_taxi_ok(t, pos, dest, c.dis, d4, res)) {
+				if (res.size() > num_res) {
+					break;
+				}
+			}
+		}
+		if (res.size() > num_res) {
+			break;
+		}
+	}
+	string out;
+	for (auto i : res) {
+		out += i + "\n";
+	}
+	return out;
+}
+
+};  // namespace taxiiii
+
+extern "C" {
+
+void taxiiii_init() {
+	taxiiii::init();
+}
+
+const char* taxiiii_find(int pos, int dest) {
+	return taxiiii::find(pos, dest).c_str();
+}
+
+};
+
+int main() {
+	taxiiii_init();
+}
+
